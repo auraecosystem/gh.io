06ccfdc8950ca0dd6fc8d085585016ae3d7df50a
auraecosystem <sandraisom@icloud.com>
2026-05-30 03:12:34 +0100

    Create Webpack.config.js

---
+++ esm/Webpack.config.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    output: {
+      filename: "./dist-first.js",
+    },
+    name: "first",
+    entry: "./src/first.js",
+    mode: "development",
+  },
+  {
+    output: {
+      filename: "./dist-second.js",
+    },
+    name: "second",
+    entry: "./src/second.js",
+    mode: "development",
+  },
+  {
+    output: {
+      filename: "./dist-third.js",
+    },
+    name: "third",
+    entry: "./src/third.js",
+    mode: "none",
+    stats: "verbose",
+  },
+];
