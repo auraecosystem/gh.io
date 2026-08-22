6906fb5a7b6aa8cc6ca88a986a3145c773110286
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-23 00:39:10 +0100

    Update and rename bash to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,7 @@
-python3 -m http.server 8000
+python3 -m http.server 8080
 SYMLINK=<symlink>
 MONOPREFIX=/Library/Frameworks/Mono.framework/Versions/$SYMLINK
 echo "##vso[task.setvariable variable=DYLD_FALLBACK_LIBRARY_PATH;]$MONOPREFIX/lib:/lib:/usr/lib:$DYLD_LIBRARY_FALLBACK_PATH"
 echo "##vso[task.setvariable variable=PKG_CONFIG_PATH;]$MONOPREFIX/lib/pkgconfig:$MONOPREFIX/share/pkgconfig:$PKG_CONFIG_PATH"
 echo "##vso[task.setvariable variable=PATH;]$MONOPREFIX/bin:$PATH"
+token=`ibmcloud iam oauth-tokens --output json | jq .iam_token -r`
