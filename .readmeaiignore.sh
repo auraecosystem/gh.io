0024bb82169392713060856bf611f774f03bd824
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-23 05:50:11 +0100

    Fix repository URL in .readmeaiignore.sh

---
+++ .readmeaiignore.sh
@@ -0,0 +1,9 @@
+          $ readmeai --repository https://github.com/olliefr/docker-gs-ping \
+           --badge-color 00ADD8 \
+           --badge-style for-the-badge \
+           --header-style modern \
+           --navigation-style roman
+pip install -U readmeai
+pip install "readmeai[anthropic,google-generativeai]"
+readmeai --repository https://github.com/auraecosystem/web4-OS.git
+readmeai -V
